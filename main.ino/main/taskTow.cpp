badf22618a30248d0f02adec1c2d352de53e6b3b
Alhaytham <asalhaytham@gmail.com>
2017-06-15 20:13:46 +0100

    changing the timing for in taskTow

--- main.ino/main/taskTow.cpp
+++ main.ino/main/taskTow.cpp
@@ -5,14 +5,14 @@ void TaskTwo(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
 
-  // initialize digital pin 13 as an output.
+  // initialize digital pin 12 as an output.
   pinMode(12, OUTPUT);
 
   for (;;) // A Task shall never return or exit.
   {
     digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+    vTaskDelay( 3000 / portTICK_PERIOD_MS ); // wait for one second
     digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+    vTaskDelay( 3000 / portTICK_PERIOD_MS ); // wait for one second
   }
 }
