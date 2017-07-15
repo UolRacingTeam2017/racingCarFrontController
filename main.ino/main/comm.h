f288ae019c16c9e8d712bc643d34805242ee1948
Alhaytham <asalhaytham@gmail.com>
2017-07-15 21:41:12 +0100

    we do not need messge to be external variable

--- main.ino/main/comm.h
+++ main.ino/main/comm.h
@@ -6,7 +6,5 @@ void ReceiveMessage(void  *pvParameters );
 void sendMessage(void  *pvParameters );
 void testCan(void  *pvParameters );
 void deliverMessage(void) ;
-extern int csValue, imdFail, csFail;  // these values are read by tasks that reside at this node.
-extern int teValue; // values send from far node/s
-extern unsigned char messageToSend[8], messageReceived[8];
 #endif
+
